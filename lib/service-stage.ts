3d4ab452bb7a55c112974d14b87f735e8952196e
Bernhard <bernhard@appointmed.com>
2022-01-04 23:27:37 +0100

    fix service stack naming

--- lib/service-stage.ts
+++ lib/service-stage.ts
@@ -11,7 +11,7 @@ export class ServiceStage extends Stage {
 
         new ServiceStack(
             this,
-            'ServiceStack',
+            'ServiceStackCdkV2CustomBuildImage',
             properties
         );
     }
